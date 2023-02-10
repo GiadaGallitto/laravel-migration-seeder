b98d983e97b6ba2b034a9ec8fcfa98af4ffecd40
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-10 15:53:57 +0100

    Create structure of migration trains

--- database/migrations/2023_02_10_143823_trains.php
+++ database/migrations/2023_02_10_143823_trains.php
@@ -13,7 +13,19 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('trains', function (Blueprint $table){
+            $table->id();
+            $table->string('Azienda', 255);
+            $table->string('Stazione di partenza', 255);
+            $table->string('Stazione di arrivo', 255);
+            $table->dateTime('Orario di partenza');
+            $table->dateTime('Orario di arrivo');
+            $table->string('Codice treno', 255);
+            $table->tinyInteger('Numero carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('trains');
     }
 };
